ed907fa3b010bd369d47bac5a153ab155c887d2c
ConorMcNelis <60985221+ConorMcNelis@users.noreply.github.com>
2020-11-24 12:35:07 +0000

    Owner.java v1.0
    This is just the data fields and getter/setter methods along with a constructor for this class. Subject to change

--- Current code todo/Owner.java
+++ Current code todo/Owner.java
@@ -1,11 +1,36 @@
-
 public class Owner {
- - name : String
- - currentAddress : String
- - cuurentPostcode : String
- 
- ------------------------------------------
- + constructor
- + all getters and setters that are necessary
- + 
+
+	private String name;
+	private String currentAddress;
+	private String currentPostcode;
+
+	public Owner(String name1, String currentAddress1, String currentPostcode1) {
+		this.setName(name1);
+		this.setCurrentAddress(currentAddress1);
+		this.setCurrentPostcode(currentPostcode1);
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCurrentAddress() {
+		return currentAddress;
+	}
+
+	public void setCurrentAddress(String currentAddress) {
+		this.currentAddress = currentAddress;
+	}
+
+	public String getCurrentPostcode() {
+		return currentPostcode;
+	}
+
+	public void setCurrentPostcode(String currentPostcode) {
+		this.currentPostcode = currentPostcode;
+	}
 }
