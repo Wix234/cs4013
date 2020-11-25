97c323e9431bc38b7bd46ff691d597d40e048013
Wix234 <61157446+Wix234@users.noreply.github.com>
2020-11-25 15:46:50 +0000

    Update Property.java

--- Current code todo/Property.java
+++ Current code todo/Property.java
@@ -1,4 +1,77 @@
-
 public class Property {
-
-}
+	private String address;
+	private String eircode;
+	private double estMarketVal;
+	private int location;
+	private boolean privateResidence;
+	
+	public Property(String address, String eircode, double estMarketVal,int location, boolean privateResidence){
+		setAddress(address);
+		setEircode(eircode);
+		setEstMarketVal(estMarketVal);
+		setLocation(location); 
+		setPrivateResidence(privateResidence);
+	}
+	
+	public String getAddress() {
+		return address;
+	}
+	
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	public String getEircode() {
+		return eircode;
+	}
+	
+	public void setEircode(String eircode) {
+		this.eircode = eircode;
+	}
+	
+	public double getEstMarketVal() {
+		return estMarketVal;
+	}
+	
+	public void setEstMarketVal(double estMarketVal) {
+		this.estMarketVal = estMarketVal;
+	}
+	
+	public int getLocation() {
+		return location;
+	}
+	
+	public void setLocation(int location) {
+		this.location = location;
+	}
+	
+	public boolean isPrivateResidence() {
+		return privateResidence;
+	}
+	
+	public void setPrivateResidence(boolean privateResidence) {
+		this.privateResidence = privateResidence;
+	}
+	
+	public String toString(){
+		String temp;
+		if (privateResidence == true){
+			temp = "Yes";
+		}else {
+			temp = "No";
+		}
+		String temp2 = null;
+		if(location == 1){
+			temp2 = "City";
+		}else if(location == 2){
+			temp2 = "Large town";
+		}else if(location == 3){
+			temp2 = "Small town";
+		}else if(location == 4){
+			temp2 = "Village";
+		}else if(location == 5){
+			temp2 = "Countryside";
+		}
+		return ("Address:\n" + address + "\n" + eircode + "\n\nEstimated market value: " + estMarketVal
+				+ "\nLocation type: " + temp2 + "\nPrinciple private Residence: " + temp);
+	}
