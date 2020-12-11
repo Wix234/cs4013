8a40ac4709f5cd40c926c44908e450ce0eef4dd0
Benjamin Setterfield <57184543+BenPSetterfield@users.noreply.github.com>
2020-12-11 15:11:01 +0000

    Updating App.java
    Converted the functionality of the button into a lambda expression, increased the size of the window. I have attempted the use of borderpane and movement of the button unsuccessfully.

--- Current code todo/JavaFXTestProject/src/App.java
+++ Current code todo/JavaFXTestProject/src/App.java
@@ -4,9 +4,13 @@ import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 
-public class App extends Application implements EventHandler<ActionEvent> {
+public class App extends Application {
 
 
     Button button;
@@ -16,27 +20,28 @@ public class App extends Application implements EventHandler<ActionEvent> {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        primaryStage.setTitle("Title of the window");
+        primaryStage.setTitle("Property Charge Management System");
 
+        BorderPane border = new BorderPane();
+        HBox hbox = addHBox();
+        border.setTop(hbox);
+        border.setLeft(addVBox());
+        addStackPane(hbox);  
+        
         button = new Button();
-        button.setText("Click me");
+        button.setText("Calculate Tax");
+        button.setLayoutX(70);
+        button.setLayoutY(100);
 
-        button.setOnAction(this);
+        button.setOnAction(e -> System.out.println("Result here"));
 
         StackPane layout = new StackPane();
         layout.getChildren().add(button);
 
-        Scene scene = new Scene(layout, 300, 250);
+        Scene scene = new Scene(layout, 500, 600);
         primaryStage.setScene(scene);
         primaryStage.show();
 
-    }
-
-    @Override
-    public void handle(ActionEvent event) {
-        if(event.getSource() == button){
-            System.out.println("Hello World");
-        }
     }
     
 }
