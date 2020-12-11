fce646a02cdea64236b6c105b76cdd2e07dc3b4d
Wix234 <61157446+Wix234@users.noreply.github.com>
2020-12-11 20:55:30 +0000

    Added in csv reader for taxes file

--- Current code todo/Main.java
+++ Current code todo/Main.java
@@ -1,4 +1,6 @@
 
+
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -8,18 +10,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-public class TestProperties {
+
+public class Main {
 	static List<Owner> owners = new ArrayList<>();
 	static List<Property> properties = new ArrayList<>();
-	private static Owners owner;
-	public static void main(String[] args) {
+	static List<Tax> taxes = new ArrayList<>();
+	private static Owners owner; 
+	public static void main(String[] args) throws IOException {
 	        owner = new Owners();
 	        readOwnersFromCSV();
 	        readPropertiesFromCSV();
+	        readTaxesFromCSV();
 	        addProperties();
+	        addTaxes();
 	        
-	        System.out.println(owner.toString());
+	        //System.out.println(owner.toString());
 	        //System.out.println(properties.toString());
+	        OwnersScreen menu = new OwnersScreen();
+			menu.run(owner);
 	    }
     
 	    private static void readOwnersFromCSV() {
@@ -79,7 +87,48 @@ public class TestProperties {
 	        String PR = details[5];
 	        int year = Integer.parseInt(details[6]);
 	        return new Property(name, address, eircode, price, location, PR, year);
+	    }  
+	    
+	    private static void readTaxesFromCSV() {
+	        String csvFile = "taxes.csv";
+	        Path pathToFile = Paths.get(csvFile);
+	        try (BufferedReader br = Files.newBufferedReader(pathToFile)) {
+	            String line = br.readLine();
+	            while (line != null) {
+	                String[] attributes = line.split(",");
+	                Tax temp = createTax(attributes);
+	                taxes.add(temp);
+	                line = br.readLine();
+	            }
+	        } catch (IOException ioe) {
+	            ioe.printStackTrace();
+	        }
+	    }
+	    public static void addTaxes(){
+	    	for (int i = 0; i < properties.size(); i ++ ){
+    			for (int j = 0; j < taxes.size(); j ++ ){
+    				if (properties.get(i) == taxes.get(j).getP()){
+    					properties.get(i).addTax(taxes.get(j));
+    				}	
+    			}
+    		}
+	    }
+	    private static Tax createTax(String[] details) {
+	    	String name = details[0];
+	    	int year = Integer.parseInt(details[1]);
+	    	int yearPaid;
+	        if (Integer.parseInt(details[2]) != 0){
+	        	yearPaid = Integer.parseInt(details[2]);
+	        }else{
+	        	yearPaid = 0;
+	        }
+	        Property p = null;
+	        for (int i = 0; i <properties.size(); i++){
+	        	if (properties.get(i).getAddress().equalsIgnoreCase(name)){
+	        		p = properties.get(i);
+	        	}
+	        }
+	        return new Tax(p, year, yearPaid);
 	    }    
 
 }
-
