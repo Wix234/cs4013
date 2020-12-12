51592283dce2638d14e1ce4d18685cff365e9365
Benjamin Setterfield <57184543+BenPSetterfield@users.noreply.github.com>
2020-12-12 23:41:53 +0000

    Extensive change to App
    I have re-evaluated what work I had done for the GUI and I think this is the best way for the "Sign in" and display of information. It is not functional as of yet but close to

--- Current code todo/JavaFXTestProject/src/App.java
+++ Current code todo/JavaFXTestProject/src/App.java
@@ -9,39 +9,79 @@ import javafx.stage.Stage;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.geometry.Insets;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
 
 public class App extends Application {
 
+    String enteredName;
+    Scene scene1;
+    Stage primaryStage;
+    String[] Owners = {"Patrick", "Admin"};
+    String[] Pproperties = {"Property1", "Property2"};
+    String[] properties = {"Property1", "Property2", "Property3"};
+    BorderPane border;
+    Button prop1;
 
-    Button button;
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        //BorderPane is the layout we will use
+        border = new BorderPane();
+
+        //CrTextFieldeate and prepare the spacing for the first menu. (Sign in)
+        HBox hbox = new HBox();
+        hbox.setPadding(new Insets(15, 12, 15, 12));
+        hbox.setSpacing(10);
+        border.setTop(hbox);
+
+        //Create and prepare the spacing for the left side menu (Used also in second stage)
+        VBox vbox = new VBox();
+        vbox.setPadding(new Insets(15, 12, 15, 12));
+        vbox.setSpacing(10);
+        border.setLeft(vbox);
+
+        //Create the button for Sign in on the first Stage
+        Button signIn = new Button();
+        signIn.setText("Sign in");
+
+        //Prepared the entered String for later use
+        TextField userName = new TextField();
+
+        //Proccessing event for sign in
+        signIn.setOnAction(e -> {
+            for(int i = 0; i < Owners.length; i++){
+                if(userName.getText() == Owners[i]){
+                    vbox.getChildren().addAll(prop1);
+                    }
+            
+                }
+            }
+
+        );
+
+        //Create the Text objects
+        Text prompt = new Text("Enter Name: ");
+        Text info = new Text("Please enter your name above to sign in.");
+
+
+        //Add the objects to the appropriate areas
+        hbox.getChildren().addAll(prompt, userName, signIn);
+        vbox.getChildren().addAll(info);
+
+        //Add the Tile
+        primaryStage.setTitle("Property Charge Management System");
+
+        //Prepare the first and second scene
+        scene1 = new Scene(border, 750, 600);
+        primaryStage.setScene(scene1);
+
+        //Show the scene
+        primaryStage.show();
+    }
+    
     public static void main(String[] args){
         launch(args);
     }
 
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        primaryStage.setTitle("Property Charge Management System");
-
-        BorderPane border = new BorderPane();
-        HBox hbox = addHBox();
-        border.setTop(hbox);
-        border.setLeft(addVBox());
-        addStackPane(hbox);  
-        
-        button = new Button();
-        button.setText("Calculate Tax");
-        button.setLayoutX(70);
-        button.setLayoutY(100);
-
-        button.setOnAction(e -> System.out.println("Result here"));
-
-        StackPane layout = new StackPane();
-        layout.getChildren().add(button);
-
-        Scene scene = new Scene(layout, 500, 600);
-        primaryStage.setScene(scene);
-        primaryStage.show();
-
-    }
-    
 }
