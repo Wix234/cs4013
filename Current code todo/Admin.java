468f6b90e90c7e9a8ffee1e6e027747a1b58b145
seán gothard <61392349+TheLegendaryLemon@users.noreply.github.com>
2020-12-13 15:43:58 +0000

    Create Admin.java

---
+++ Current code todo/Admin.java
@@ -0,0 +1,16 @@
+
+public class Admin {
+		private String adminName;
+
+		public Admin(String adminName) {
+			this.setAdminName(adminName);
+		}
+
+		public String getAdminName() {
+			return adminName;
+		}
+
+		public void setAdminName(String adminName) {
+			this.adminName = adminName;
+		}
+}
